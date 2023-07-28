8eb2ccf6a95b3788b02c355dd5c584cc6106ba61
Rupa830904 <rupa.kabyik@gmail.com>
2023-07-28 16:16:34 +0000

    Added Models and built 'studyalong' admin page

--- studyalong/admin.py
+++ studyalong/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Course, Interest
+#from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+@admin.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'starting_on')
+    search_fields = ['title', 'content']
+    list_filter = ('title','starting_on')
+    prepopulated_fields = {'slug': ('title',)}
+    #summernote_fields = ('content',)
+
+@admin.register(Interest)
+
+class InterestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'course','created_on', 'acknowledged')
+    list_filter = ('acknowledged', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['acknowledge_interests']
+
+    def acknowledge_interests(self, request, queryset):
+        queryset.update(approved=True)
--- studyalong/models.py
+++ studyalong/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    #excerpt = models.TextField(blank=True)
+    #updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    starting_on = models.DateField(null=True)
+    #status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-starting_on"]
+
+    def __str__(self):
+        return self.title
+
+class Interest(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE,
+                             related_name="interests")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateField(null=True)
+    acknowledged = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
