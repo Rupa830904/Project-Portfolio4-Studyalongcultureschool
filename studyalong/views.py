b10c20a2eec1db0b8e2b36eb9e0d76a1e74cd399
Rupa830904 <rupa.kabyik@gmail.com>
2023-08-13 11:34:34 +0000

    Add course details view and url

--- studyalong/views.py
+++ studyalong/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 from .models import Course
 
 # Create your views here.
@@ -8,3 +9,8 @@ class CourseList(generic.ListView):
     model = Course
     queryset = Course.objects.order_by("-starting_on")
     template_name = "index.html"
+
+class CourseDetail(DetailView):
+    model = Course
+    queryset = Course.objects.order_by("-starting_on")
+    template_name = "course_detail.html"
